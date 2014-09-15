5680da9a5dae2cd21dd42aa956a169b17daff19d
Isabelle-Angrignon <isabelle.angrignon@hotmail.ca>
2014-09-15 13:22:26 -0400

    début controleur serveur

---
+++ ControleurServeur/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlotteDLL;
+
+namespace ControleurServeur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flotte joueur1 = new Flotte();
+            Flotte joueur2 = new Flotte();
+            bool partieEstFinie = false;
+            bool tourPremierJoueur = true;
+
+
+            //placez btx j1...
+            //mise a jour grille 1
+            //placez btx j2...
+            //mise a jour grille 2
+
+            //tant que partie 
+            while (!partieEstFinie)
+            {
+                //demander de lancer une torpille;
+                //analyser le coup
+                    //vérifier si touché
+                        //vérifier si coulé
+                            //vérifier si gagné
+                //rafraichir les grilles des joueurs
+
+
+
+                //alterner joueur;
+                tourPremierJoueur = !tourPremierJoueur;
+            }
+            //afficher gagnant
+        }    
+    }
+}
