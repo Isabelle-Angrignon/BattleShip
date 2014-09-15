5b9896d94461fe462f0b0fdc7ccb295af72abe16
Ariekor <851s2001@gmail.com>
2014-09-15 15:59:48 -0400

    shit 4 sync a faire

--- CommServeur/CommServeur/CommunicationServeur.cs
+++ CommServeur/CommServeur/CommunicationServeur.cs
@@ -11,10 +11,14 @@ namespace CommServeur
     class CommunicationServeur
     {
         Socket sockConn;
-        Socket sockJoueur1;
-        Socket sockJoueur2;
+        Socket sockJoueur1 = null;
+        Socket sockJoueur2 = null;
         private int nbJoueurConnecter;
         static byte[] Buffer { get; set; }
+        bool J1Connecter = false;
+        bool J2Connecter = false;
+        bool J1EnvoiBloque = false;
+        bool J2EnvoiBloque = false;
 
         /// <summary>
         /// Seulement entre 0 et 2 
@@ -42,16 +46,28 @@ namespace CommServeur
         /// <param name="NbJoueurConnecter">Dis si on veut connecter un joueur ou deux </param>
         public void Connection(int NbJoueurAConnecter)
         {
-            if (NbJoueurAConnecter >= 1 && NbJoueurAConnecter <= 2)
+            if(J1Connecter != null && J2Connecter != null )
+            {
+                throw new Exception("Les joueurs sont déjà connecter ! ");
+            }
+            else if (NbJoueurAConnecter >= 1 && NbJoueurAConnecter <= 2 && sockJoueur1 != null)
             {
                 sockConn.Listen(1);
                 sockJoueur1 = sockConn.Accept();
+                J1Connecter = true;
 
                 if (NbJoueurAConnecter == 2)
                 {
                     sockConn.Listen(1);
                     sockJoueur2 = sockConn.Accept();
+                    J2Connecter = true;
                 }
+            }
+            else if (NbJoueurAConnecter >= 1 && NbJoueurAConnecter <= 2)
+            {
+                sockConn.Listen(1);
+                sockJoueur2 = sockConn.Accept();
+                J2Connecter = true;
             }
             else
             {
@@ -82,6 +98,29 @@ namespace CommServeur
         /// <param name="numeroJoueur">Le joueur auquel il faut envoyer le message</param>
         /// <param name="Message"> Le message qu'il faut envoyer </param>
         public void EnvoyerMessage(int numeroJoueur, String Message)
+        {
+            if (numeroJoueur == 1)
+            {
+                EnvoyerMessage(sockJoueur1, Message);
+            }
+            if (numeroJoueur == 2)
+            {
+
+                EnvoyerMessage(sockJoueur2, Message);
+            }
+            if ( numeroJoueur != 1 || numeroJoueur != 2)
+            {
+                throw new Exception("Le numero de joueur peut seulement être 1 ou 2");
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sock">Le socket auquel il faut envoyer le message</param>
+        /// <param name="Message">Le message a envoyer</param>
+        private void EnvoyerMessage(Socket sock, String Message)
         {
 
         }
